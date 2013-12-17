6f1df26c6a5add7bbe28be077c5c48d37d37ee0a
İbrahim Sidat <sidati@gmail.com>
2013-12-17 17:58:36 +0200

    DrawObject Size

--- s/data.js
+++ s/data.js
@@ -1,6 +1,5 @@
 ﻿var Data = {
 	Buildings: [
-		{ name: 'Home1', xsize: 1, ysize: 1, imageSrc: 'img/home1.png' },
-		{ name: 'Home2', xsize: 1, ysize: 1, imageSrc: 'img/home2.png' }
+		{ name: 'Home1', xsize: 1, ysize: 1, height: 30, imageSrc: 'img/home1.png' }
 	]
 };
--- s/game.js
+++ s/game.js
@@ -32,7 +32,7 @@
 			var canvasElement = document.getElementById('maincanvas');
 			sheetengine.scene.init(canvasElement, { w:Game.Config.CanvasWidth, h:Game.Config.CanvasHeight });
 			this.CreateBaseSheets();
-			this.CreateDummyObject();			
+			this.DrawObjects();			
 			this.DrawScene(true);
 		},
 		CreateBaseSheets: function() {
@@ -47,11 +47,11 @@
 			sheetengine.calc.calculateChangedSheets();
 			sheetengine.drawing.drawScene(full);
 		},
-		CreateDummyObject: function() {
-			var sheet1 = new sheetengine.Sheet({ x:0, y:0, z:Game.Config.BaseSheetSize/2 }, { alphaD:0, betaD:0, gammaD:0 }, { w:Game.Config.BaseSheetSize, h:Game.Config.BaseSheetSize });
+		DrawObjects: function() {
+			var sheet1 = new sheetengine.Sheet({ x:0, y:Game.Config.BaseSheetSize/2, z:15 }, { alphaD:0, betaD:0, gammaD:0 }, { w:Game.Config.BaseSheetSize, h:30 });
 			sheet1.context.fillStyle = "#F00";
-			sheet1.context.fillRect(0, 0, Game.Config.BaseSheetSize, Game.Config.BaseSheetSize);
-			sheet1.context.clearRect(10, 10, 30, 30);
+			sheet1.context.fillRect(0, 0, Game.Config.BaseSheetSize, 30);
+			sheet1.context.clearRect(10, 10, 30, 10);
 			//var obj = new sheetengine.SheetObject({x:0,y:0,z:0}, {alphaD:0,betaD:0,gammaD:0}, [sheet1], {w:Game.Config.BaseSheetSize,h:Game.Config.BaseSheetSize});
 		}
 	}
