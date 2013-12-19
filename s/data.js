6398a6a3e4ab44b8a26d22418c5768e23c3d96ba
İbrahim Sidat <sidati@gmail.com>
2013-12-19 19:44:31 +0200

    Buildings

--- s/data.js
+++ s/data.js
@@ -1,11 +1,11 @@
 ﻿var Data = {
 	Buildings: [
-		{ name: 'Home1', x:3, y:7, xsize: 1, ysize: 1, height: 100, imageSrc: 'img/home1.png' },
-		{ name: 'Home2', x:5, y:4, xsize: 1, ysize: 1, height: 50, imageSrc: 'img/home2.png' },
-		{ name: 'Home1', x:0, y:6, xsize: 1, ysize: 1, height: 40, imageSrc: 'img/home1.png' },
-		{ name: 'Home1', x:9, y:4, xsize: 1, ysize: 1, height: 30, imageSrc: 'img/home1.png' },
-		{ name: 'Home1', x:6, y:3, xsize: 1, ysize: 1, height: 80, imageSrc: 'img/home1.png' },
-		{ name: 'Home1', x:7, y:8, xsize: 1, ysize: 1, height: 70, imageSrc: 'img/home1.png' },
-		{ name: 'Home1', x:8, y:7, xsize: 1, ysize: 1, height: 50, imageSrc: 'img/home1.png' }
+		{ name: 'Home1', x:3, y:7, xsize: 1, ysize: 1, height: 30, imageSrc: 'img/home1.png' },
+		{ name: 'Home2', x:1, y:3, xsize: 1, ysize: 1, height: 60, imageSrc: 'img/home2.png' },
+		{ name: 'Home1', x:0, y:9, xsize: 1, ysize: 1, height: 20, imageSrc: 'img/home1.png' },
+		{ name: 'Home1', x:13, y:4, xsize: 1, ysize: 1, height: 70, imageSrc: 'img/home1.png' },
+		{ name: 'Home1', x:6, y:3, xsize: 1, ysize: 1, height: 40, imageSrc: 'img/home1.png' },
+		{ name: 'Home1', x:11, y:10, xsize: 1, ysize: 1, height: 20, imageSrc: 'img/home1.png' },
+		{ name: 'Home1', x:4, y:13, xsize: 1, ysize: 1, height: 40, imageSrc: 'img/home1.png' }
 	]
 };
--- s/game.js
+++ s/game.js
@@ -22,11 +22,11 @@
 		}
     },
 	Config: {
-		BaseSheetSize: 30,
-		MapSize: 14,
+		BaseSheetSize: 40,
+		MapSize: 15,
 		CanvasWidth: 1800,
 		CanvasHeight: 900,
-		HomePadding: 10
+		HomePadding: 8
     },
 	SheetEngine: {
 		Init: function() {
@@ -39,7 +39,11 @@
 		CreateBaseSheets: function() {
 			for (var x=0; x<Game.Config.MapSize; x++) {
 			  for (var y=0; y<Game.Config.MapSize; y++) {
-				var basesheet = new sheetengine.BaseSheet({ x:x*Game.Config.BaseSheetSize, y:y*Game.Config.BaseSheetSize, z:0 }, { alphaD:90, betaD:0, gammaD:0 }, { w:Game.Config.BaseSheetSize, h:Game.Config.BaseSheetSize });
+				var basesheet = new sheetengine.BaseSheet(
+					{ x:x*Game.Config.BaseSheetSize, y:y*Game.Config.BaseSheetSize, z:0 },
+					{ alphaD:90, betaD:0, gammaD:0 },
+					{ w:Game.Config.BaseSheetSize, h:Game.Config.BaseSheetSize }
+				);
 				basesheet.color = Game.Helper.Color.PickRandomBaseSheetColor();
 			  }
 			}
@@ -50,14 +54,49 @@
 		},
 		DrawObjects: function() {
 			Data.Buildings.forEach(function(building) {
-				var sheet = new sheetengine.Sheet(
-					{ x:Game.Config.BaseSheetSize*building.x, y:Game.Config.BaseSheetSize/2+Game.Config.BaseSheetSize*building.y, z:building.height/2 },
+				var southSheet = new sheetengine.Sheet(
+					{ x:Game.Config.BaseSheetSize*building.x, y:Game.Config.BaseSheetSize*(building.y+0.5), z:building.height/2 },
 					{ alphaD:0, betaD:0, gammaD:0 },
 					{ w:Game.Config.BaseSheetSize, h:building.height }
 				);
-				sheet.context.fillStyle = "#F00";
-				sheet.context.fillRect(0, 0, Game.Config.BaseSheetSize, building.height);
-				sheet.context.clearRect(Game.Config.HomePadding, Game.Config.HomePadding, Game.Config.BaseSheetSize-2*Game.Config.HomePadding, building.height-2*Game.Config.HomePadding);
+				southSheet.context.fillStyle = "#FF0";
+				southSheet.context.fillRect(0, 0, Game.Config.BaseSheetSize, building.height);
+				southSheet.context.fillStyle = "#588";
+				southSheet.context.fillRect(Game.Config.HomePadding, Game.Config.HomePadding, Game.Config.BaseSheetSize-2*Game.Config.HomePadding, building.height-2*Game.Config.HomePadding);
+				
+				var westSheet = new sheetengine.Sheet(
+					{ x:Game.Config.BaseSheetSize*(building.x-0.5), y:Game.Config.BaseSheetSize*(building.y), z:building.height/2 },
+					{ alphaD:0, betaD:0, gammaD:90 },
+					{ w:Game.Config.BaseSheetSize, h:building.height }
+				);
+				westSheet.context.fillStyle = "#FF0";
+				westSheet.context.fillRect(0, 0, Game.Config.BaseSheetSize, building.height);
+				
+				var northSheet = new sheetengine.Sheet(
+					{ x:Game.Config.BaseSheetSize*building.x, y:Game.Config.BaseSheetSize*(building.y-0.5), z:building.height/2 },
+					{ alphaD:0, betaD:0, gammaD:0 },
+					{ w:Game.Config.BaseSheetSize, h:building.height }
+				);
+				northSheet.context.fillStyle = "#FF0";
+				northSheet.context.fillRect(0, 0, Game.Config.BaseSheetSize, building.height);
+				
+				var eastSheet = new sheetengine.Sheet(
+					{ x:Game.Config.BaseSheetSize*(building.x+0.5), y:Game.Config.BaseSheetSize*(building.y), z:building.height/2 },
+					{ alphaD:0, betaD:0, gammaD:90 },
+					{ w:Game.Config.BaseSheetSize, h:building.height }
+				);
+				eastSheet.context.fillStyle = "#FF0";
+				eastSheet.context.fillRect(0, 0, Game.Config.BaseSheetSize, building.height);
+				eastSheet.context.fillStyle = "#844";
+				eastSheet.context.fillRect(Game.Config.HomePadding, building.height-Game.Config.HomePadding, Game.Config.BaseSheetSize-2*Game.Config.HomePadding, Game.Config.HomePadding);
+				
+				var roofSheet = new sheetengine.Sheet(
+					{ x:Game.Config.BaseSheetSize*(building.x), y:Game.Config.BaseSheetSize*(building.y), z:building.height },
+					{ alphaD:90, betaD:0, gammaD:0 },
+					{ w:Game.Config.BaseSheetSize, h:Game.Config.BaseSheetSize }
+				);
+				roofSheet.context.fillStyle = "#F00";
+				roofSheet.context.fillRect(0, 0, Game.Config.BaseSheetSize, Game.Config.BaseSheetSize);
 			});
 			//var obj = new sheetengine.SheetObject({x:0,y:0,z:0}, {alphaD:0,betaD:0,gammaD:0}, [sheet1], {w:Game.Config.BaseSheetSize,h:Game.Config.BaseSheetSize});
 		}
